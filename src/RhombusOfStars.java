83d31c7905865ab95362324f43eef4fe47035a0d
Kris13081 <kristianstefanov0@gmail.com>
2022-10-26 10:07:59 +0300

    Ex 1 RhombusOfStars done

--- src/RhombusOfStars.java
+++ src/RhombusOfStars.java
@@ -1,2 +1,46 @@
+import java.util.Scanner;
+
 public class RhombusOfStars {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+
+        printTopPart(n);
+
+        printMiddlePrt(n);
+
+        printBottomPart(n);
+
+    }
+
+    private static void printTopPart(int n) {
+        for (int row = 1; row <= n - 1 ; row++) {
+            printRow(n, row);
+        }
+    }
+
+    private static void printMiddlePrt(int n) {
+        for (int star = 1; star <= n; star++) {
+            System.out.print("* ");
+        }
+        System.out.println();
+    }
+
+    private static void printBottomPart(int n) {
+        for (int row = n - 1; row >= 1 ; row--) {
+            printRow(n,row);
+        }
+    }
+
+    public static void printRow(int n, int row){
+        for (int space = 1; space <= n - row; space++) {
+            System.out.print(" ");
+        }
+        for (int star = 1; star <= row; star++) {
+            System.out.print("* ");
+        }
+        System.out.println();
+    }
 }
