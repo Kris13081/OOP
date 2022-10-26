3add65cc4295c720d631e897c3aecb4f2f1ac00a
Kris13081 <kristianstefanov0@gmail.com>
2022-10-26 14:50:06 +0300

    All ex from WorkingWithAbstraction theme are done.

---
+++ src/WorkingWithAbstraction/HotelReservation/Main.java
@@ -0,0 +1,22 @@
+package WorkingWithAbstraction.HotelReservation;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String input = scanner.nextLine();
+        String[] splitInput = input.split("\\s+");
+
+        double pricePerDay = Double.parseDouble(splitInput[0]);
+        int numberOfDays = Integer.parseInt(splitInput[1]);
+        Season season = Season.valueOf(splitInput[2].toUpperCase());
+        DiscountType discountType = DiscountType.valueOf(splitInput[3].toUpperCase());
+
+        double holidayPrice =  PriceCalculator.calculateHolidayPrice(pricePerDay, numberOfDays, season, discountType);
+
+        System.out.printf("%.2f", holidayPrice);
+
+    }
+}
